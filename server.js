c3b22778377bd61d12537dacb90d84869a5a4d89
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-10-12 12:13:56 -0400

    Another attempt at database connecting

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const bcrypt = require('bcrypt');
 //___________________
 // Allow use of Heroku's port or your own local port, depending on the environment
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + 'CheckMeIn';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/' + 'check-me-in';
 
 // Connect to Mongo
 mongoose.connect(MONGODB_URI , {useNewUrlParser: true});
