c4be351121c523948b41f4cb32213f1982363313
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-10-11 22:27:21 -0400

    creation of seed

--- controllers/flights.js
+++ controllers/flights.js
@@ -10,18 +10,43 @@ flights.get('/' , (req, res) => {
   res.render('index.ejs');
 });
 
+flights.get('/new', (req, res) => {
+  res.render('new.ejs');
+})
 
+flights.delete('/:id', (req, res) => {
+  Flight.findByIdAndRemove(req.params.id, (req, data) => {
+    res.redirect('/flights');
+  })
+})
 
+flights.get('/:id/edit', (req, res) => {
+  Flights.findById(req.params.id, (err, locateFlights) => {
+    res.render('edit.ejs', {
+      flight: locateFlights
+    });
+  })
+})
 
+flights.put('/:id', (req, res) => {
+  Flights.findByIdAndUpdate(req.params.id, {new: true}, (err, updateModel) => {
+    res.redirect('/flights');
+  })
+})
 
+flights.post('/', (req, res) => {
+  Flights.create(req.body, (err, createdFlights) => {
+    res.redirect('/flights');
+  })
+})
 
-
-
-
-
-
-
-
+flights.get('/:id', (req, res) => {
+  Flights.findById(req.params.id, (err, locateFlights) => {
+    res.render('show.ejs', {
+      flight: locateFlights
+    });
+  })
+})
 
 
 
---
+++ models/seed.js
@@ -0,0 +1,25 @@
+const seedFlights = [
+        {
+          name: 'Joe Bob',
+          flightNum: 6542,
+          airlineName: 'Southwest',
+          confirmationNum: 16568463654,
+          checkInTime: 6
+        },
+        {
+          name: 'Mary Lou',
+          flightNum: 5436,
+          airlineName: 'United',
+          confirmationNum: 16163545,
+          checkInTime: 3
+        },
+        {
+          name: 'Jimmy John',
+          flightNum: 6498,
+          airlineName: 'Delta',
+          confirmationNum: 61846456546,
+          checkInTime: 3
+        }
+    ]
+
+module.exports = seedFlights;
