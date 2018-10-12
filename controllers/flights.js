5033514a2e6291dcc278e6c9165dbf88b58274dc
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-10-12 13:00:49 -0400

    dfdf

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,11 +8,11 @@ const flightDB = require('../models/flightDB.js');
 //___________________
 //localhost:3000
 flights.get('/', (req, res) => {
-    Flights.find({}, (error, allFlights) => {
+    // Flights.find({}, (error, allFlights) => {
         res.render('index.ejs', {
             flight: allFlights
           })
-        })
+        // })
 })
 
 flights.get('/new', (req, res) => {
