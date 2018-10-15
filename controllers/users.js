40707a34bcf75475458e8d4943151e88031348d8
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-10-15 12:48:42 -0400

    Setting up sessions files to bring in login and sign up feature

---
+++ controllers/users.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const users = express.Router()
+const User = require('../models/users.js')
+const bcrypt = require('bcrypt')
+
+users.get('/new', (req, res) => {
+  res.render('users/new.ejs')
+})
+
+users.post('/', (req, res) => {
+  console.log('before bcrypt', req.body.password);
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+  User.create(req.body, (err, createdUser) => {
+    if(err) {
+      console.log(err);
+    } console.log(createdUser);
+    res.redirect('/')
+  })
+})
+
+
+
+
+
+
+
+
+
+module.exports = users
