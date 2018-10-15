700608508ed97d535d66ae3484580db91a7ae442
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-10-15 12:00:57 -0400

    Split Name key into firstName and lastName

--- models/flights.js
+++ models/flights.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const flightSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
     flightNum: {type: Number, required: true},
     airlineName: {type: String, required: true},
     confirmationNum: {type: Number, required: true},
